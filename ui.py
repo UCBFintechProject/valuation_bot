ae415d5d351fede560cf30e5257a3c2539db68cb
pgunter25 <37387761+pgunter25@users.noreply.github.com>
2022-07-10 17:38:00 +1000

    updating currency format

--- ui.py
+++ ui.py
@@ -53,7 +53,7 @@ st.subheader("Cash")
 current_cash = df.loc[chosen_ticker].iat[6]
 current_cash_upper = current_cash * 1.5
 current_cash_lower = current_cash * 0.5
-st.write("The current cash of the chosen ticker is " + "${:0,.2f}".format(current_cash))
+st.write("The current cash of the chosen ticker is " + "${:0,.0f}".format(current_cash))
 predicted_cash = st.slider("Predicted cash reported by " + chosen_ticker , int(current_cash_lower), int(current_cash_upper), int(current_cash) )
 
 # Net receivables
