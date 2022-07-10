2a139a744d46032191bb10585a127de79f9e7ee7
pgunter25 <37387761+pgunter25@users.noreply.github.com>
2022-07-10 15:40:42 +1000

    removed example code

--- ui.py
+++ ui.py
@@ -105,7 +105,7 @@ st.write("The current cash of the chosen ticker is " + str(current_cash))
 # Market cap? 
 
  #Slider for user to choose the input for cash 
- # #num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+ # num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
 #with st.echo(code_location='below'):
 predicted_cash = st.slider("Predicted cash reported by " + chosen_ticker + ".", int(current_cash_lower), int(current_cash_upper), int(current_cash) )
@@ -115,24 +115,3 @@ predicted_cash = st.slider("Predicted cash reported by " + chosen_ticker + ".", 
 st.write('Current Share Price is' )
 st.write('The predicted value of the share price based on the selections above is ' )
 
-""" with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
- """
